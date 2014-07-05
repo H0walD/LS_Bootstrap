aee204f634693dbdbc07288c2f22ecbc19e286a3
H0walD <howald@dsl.li>
2014-07-05 21:12:37 +0200

    Revert "Ls-Bootstrap"
    This reverts commit 2f2905a783fd5301ba46a42dc23ca656203a424d.

--- include/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ( 'DBHOST', 'localhost' );   # sql host
-define ( 'DBUSER', 'howald');  # sql user
-define ( 'DBPASS', 'Amelie27uA');  # sql pass
-define ( 'DBDATE', 'ls-bootstrap');  # sql datenbank
-define ( 'DBPREF', 'ic1_'); # sql prefix
-?>
